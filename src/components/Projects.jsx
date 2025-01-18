e8c80221876d0fbf1f974dd91ffea61346641dee
Gulvaiz <gulvaizalam12@gmail.com>
2025-01-19 01:10:09 +0530

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,7 +27,7 @@ const Projects = () => {
            className="w-full lg:w-3/4 max-w-xl">
               <h6 className="mb-2 font-semibold">{project.title}</h6>
               <p className="mb-4 text-neutral-400">{project.description}</p>
-              <p><a className="mr-2 text-sm text-pink-400" target="_blank" href={PROJECTS.gitlink}>GitHub repo</a></p>
+              <p><a className="mr-2 text-sm text-pink-400" target="_blank" href={PROJECTS.gitlink}>GitHub/Link</a></p>
               {project.technologies.map((tech, index) => (
                   <span key={index} className="mr-2 rounded bg-neutral-900 px-2 -py-1 text-sm font-medium text-purple-900">{tech}</span>
               ))}
